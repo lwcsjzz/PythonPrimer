c43e3df27ab5e3c264df82636e71ee4f7da65221
lwcsjzz <liuwenchang.x@qq.com>
2020-10-14 16:19:52 +0800

    1

---
+++ 5.3.py
@@ -0,0 +1,13 @@
+from functools import reduce
+x = int(input("请输入a"))
+n = int(input("请输入n"))
+a = list()
+a = [0]*(n+1)
+a[1]=x
+for i in range(1,n):
+    a[i+1]=a[i]+x*10**i
+sum = 0
+#for i in range(1,n+1):
+#    sum+=a[i]
+sum = reduce(lambda b,c:b+c,a)
+print("sum = %d" % sum)
